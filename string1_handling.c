d8ba8c9223170efb2abcb9d60f4d01ef9b294b00
charity-m <njerycharity@gmail.com>
2023-12-15 14:18:01 -0800

    strghd

--- string1_handling.c
+++ string1_handling.c
@@ -1,85 +1,87 @@
 #include "shell.h"
-/**
- *_pts - prints an input string
- *@strg: the string to be printed
- *
- * Return: Nothing
- */
-void _pts(char *strg)
-{
-	int i = 0;
-
-	if (!strg)
-		return;
-	while (strg[i] != '\0')
-	{
-		_ptchar(strg[i]);
-		i++;
-	}
-}
 
 /**
- * _ptchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _ptchar(char c)
-{
-	static int i;
-	static char buff[WRITE_BUFF_SIZE];
-
-	if (c == BUFF_FLUSH || i >= WRITE_BUFF_SIZE)
-	{
-		write(1, buff, i);
-		i = 0;
-	}
-	if (c != BUFF_FLUSH)
-		buff[i++] = c;
-	return (1);
-}
-/**
- * _strgcpy - copies a string
- * @dstn: the destination
+ * _strcpy - copies a string
+ * @dest: the destination
  * @src: the source
  *
  * Return: pointer to destination
  */
-char *_strgcpy(char *dstn, char *src)
+char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	if (dsnt == src || src == 0)
-		return (dstn);
+	if (dest == src || src == 0)
+		return (dest);
 	while (src[i])
 	{
-		dstn[i] = src[i];
+		dest[i] = src[i];
 		i++;
 	}
-	dstn[i] = 0;
-	return (dstn);
+	dest[i] = 0;
+	return (dest);
 }
 
 /**
- * _strgdup - duplicates a string
- * @strg: the string to duplicate
+ * _strdup - duplicates a string
+ * @str: the string to duplicate
  *
  * Return: pointer to the duplicated string
  */
-char *_strgdup(const char *strg)
+char *_strdup(const char *str)
 {
 	int length = 0;
 	char *ret;
 
-	if (strg == NULL)
+	if (str == NULL)
 		return (NULL);
-	while (*strg++)
+	while (*str++)
 		length++;
 	ret = malloc(sizeof(char) * (length + 1));
 	if (!ret)
 		return (NULL);
 	for (length++; length--;)
-		ret[length] = *--strg;
+		ret[length] = *--str;
 	return (ret);
+}
+
+/**
+ *_puts - prints an input string
+ *@str: the string to be printed
+ *
+ * Return: Nothing
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(1, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
 }
