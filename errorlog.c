ef100c9cc13b9c3623240b238b47df444109bcaa
charity-m <njerycharity@gmail.com>
2023-12-15 14:59:41 -0800

    err

--- errorlog.c
+++ errorlog.c
@@ -1,23 +1,5 @@
 #include "shell.h"
-/**
- *_ptsfd - prints an input string
- * @str: the string to be printed
- * @fd: the filedescriptor to write to
- *
- * Return: the number of chars put
- */
-int _ptsfd(char *str, int fd)
-{
-	int i = 0;
 
-	if (!str)
-		return (0);
-	while (*str)
-	{
-		i += _ptsfd(*str++, fd);
-	}
-	return (i);
-}
 /**
  *_eputs - prints an input string
  * @str: the string to be printed
@@ -58,6 +40,7 @@ int _eputchar(char c)
 		buf[i++] = c;
 	return (1);
 }
+
 /**
  * _putfd - writes the character c to given fd
  * @c: The character to print
@@ -79,4 +62,24 @@ int _putfd(char c, int fd)
 	if (c != BUF_FLUSH)
 		buf[i++] = c;
 	return (1);
+}
+
+/**
+ *_putsfd - prints an input string
+ * @str: the string to be printed
+ * @fd: the filedescriptor to write to
+ *
+ * Return: the number of chars put
+ */
+int _putsfd(char *str, int fd)
+{
+	int i = 0;
+
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		i += _putfd(*str++, fd);
+	}
+	return (i);
 }
