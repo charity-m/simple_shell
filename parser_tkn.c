2d7284dd93c1d368065ebb09061d8c2c0658aed5
charity-m <njerycharity@gmail.com>
2023-12-15 14:13:02 -0800

    psrertkn

--- parser_tkn.c
+++ parser_tkn.c
@@ -1,23 +1,25 @@
 #include "shell.h"
 
+#include "shell.h"
+
 /**
- * **strg_wrd - splits a string into words. Repeat delimiters are ignored
- * @strg: the input string
- * @de: the delimeter string
+ * **strtow - splits a string into words. Repeat delimiters are ignored
+ * @str: the input string
+ * @d: the delimeter string
  * Return: a pointer to an array of strings, or NULL on failure
  */
 
-char **strg_wrd(char *strg, char *de)
+char **strtow(char *str, char *d)
 {
 	int i, j, k, m, numwords = 0;
 	char **s;
 
-	if (strg == NULL || strg[0] == 0)
+	if (str == NULL || str[0] == 0)
 		return (NULL);
-	if (!de)
+	if (!d)
 		d = " ";
 	for (i = 0; str[i] != '\0'; i++)
-		if (!is_delim(strg[i], de) && (is_delim(strg[i + 1], de) || !strg[i + 1]))
+		if (!is_delim(str[i], d) && (is_delim(str[i + 1], d) || !str[i + 1]))
 			numwords++;
 
 	if (numwords == 0)
@@ -27,10 +29,10 @@ char **strg_wrd(char *strg, char *de)
 		return (NULL);
 	for (i = 0, j = 0; j < numwords; j++)
 	{
-		while (is_delim(strg[i], de))
+		while (is_delim(str[i], d))
 			i++;
 		k = 0;
-		while (!is_delim(strg[i + k], de) && strg[i + k])
+		while (!is_delim(str[i + k], d) && str[i + k])
 			k++;
 		s[j] = malloc((k + 1) * sizeof(char));
 		if (!s[j])
@@ -41,7 +43,7 @@ char **strg_wrd(char *strg, char *de)
 			return (NULL);
 		}
 		for (m = 0; m < k; m++)
-			s[j][m] = strg[i++];
+			s[j][m] = str[i++];
 		s[j][m] = 0;
 	}
 	s[j] = NULL;
@@ -49,21 +51,21 @@ char **strg_wrd(char *strg, char *de)
 }
 
 /**
- * **strg_wrd2 - splits a string into words
- * @strg: the input string
- * @de: the delimeter
+ * **strtow2 - splits a string into words
+ * @str: the input string
+ * @d: the delimeter
  * Return: a pointer to an array of strings, or NULL on failure
  */
-char **strg_wrd2(char *strg, char de)
+char **strtow2(char *str, char d)
 {
 	int i, j, k, m, numwords = 0;
 	char **s;
 
-	if (strg == NULL || strg[0] == 0)
+	if (str == NULL || str[0] == 0)
 		return (NULL);
-	for (i = 0; strg[i] != '\0'; i++)
-		if ((strg[i] != de && strg[i + 1] == de) ||
-		    (strg[i] != de && !strg[i + 1]) || strg[i + 1] == de)
+	for (i = 0; str[i] != '\0'; i++)
+		if ((str[i] != d && str[i + 1] == d) ||
+		    (str[i] != d && !str[i + 1]) || str[i + 1] == d)
 			numwords++;
 	if (numwords == 0)
 		return (NULL);
@@ -72,10 +74,10 @@ char **strg_wrd2(char *strg, char de)
 		return (NULL);
 	for (i = 0, j = 0; j < numwords; j++)
 	{
-		while (strg[i] == de && strg[i] != de)
+		while (str[i] == d && str[i] != d)
 			i++;
 		k = 0;
-		while (strg[i + k] != de && strg[i + k] && strg[i + k] != de)
+		while (str[i + k] != d && str[i + k] && str[i + k] != d)
 			k++;
 		s[j] = malloc((k + 1) * sizeof(char));
 		if (!s[j])
@@ -86,7 +88,7 @@ char **strg_wrd2(char *strg, char de)
 			return (NULL);
 		}
 		for (m = 0; m < k; m++)
-			s[j][m] = strg[i++];
+			s[j][m] = str[i++];
 		s[j][m] = 0;
 	}
 	s[j] = NULL;
