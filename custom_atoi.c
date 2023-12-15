358555a013423dc5a52a6fc678bc7182e6ae49a8
charity-m <njerycharity@gmail.com>
2023-12-15 13:45:44 -0800

    custom

--- custom_atoi.c
+++ custom_atoi.c
@@ -1,35 +1,51 @@
 #include "shell.h"
+#include "shell.h"
+
 /**
- * is_delitre - checks if character is a delimeter
+ * interactive - returns true if shell is interactive mode
+ * @info: struct address
+ *
+ * Return: 1 if interactive mode, 0 otherwise
+ */
+int interactive(info_t *info)
+{
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+}
+
+/**
+ * is_delim - checks if character is a delimeter
  * @c: the char to check
  * @delim: the delimeter string
  * Return: 1 if true, 0 if false
  */
-int is_delitre(char c, char *delim)
+int is_delim(char c, char *delim)
 {
 	while (*delim)
 		if (*delim++ == c)
 			return (1);
 	return (0);
 }
+
 /**
- *_isalphbc - checks for alphabetic character
+ *_isalpha - checks for alphabetic character
  *@c: The character to input
  *Return: 1 if c is alphabetic, 0 otherwise
  */
 
-int _isalphbc(int c)
+int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
 }
+
 /**
- * _atoi - takes string and converts to integer
- * @s: the string to input
- * Return: 1 if '+', 0 otherwise
+ *_atoi - converts a string to an integer
+ *@s: the string to be converted
+ *Return: 0 if no numbers in string, converted number otherwise
  */
+
 int _atoi(char *s)
 {
 	int i, sign = 1, flag = 0, output;
@@ -56,14 +72,4 @@ int _atoi(char *s)
 		output = result;
 
 	return (output);
-}
-/**
- * interactive - returns true if shell is interactive mode
- * @info: struct address
- *
- * Return: 1 if interactive mode, 0 otherwise
- */
-int interactive(info_t *info)
-{
-	return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
