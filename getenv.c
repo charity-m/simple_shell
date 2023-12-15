721569be12fe5cf1c2f06162ffd938bfcae45fa0
charity-m <njerycharity@gmail.com>
2023-12-15 14:51:19 -0800

    env

--- getenv.c
+++ getenv.c
@@ -1,46 +1,22 @@
 #include "shell.h"
+
 /**
- * _setenv - Initialize a new environment variable,
- *             or modify an existing one
+ * get_environ - returns the string array copy of our environ
  * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
- * @var: the string env var property
- * @value: the string env var value
- *  Return: Always 0
+ *          constant function prototype.
+ * Return: Always 0
  */
-int _setenv(info_t *info, char *var, char *value)
+char **get_environ(info_t *info)
 {
-	char *buff = NULL;
-	list_t *node;
-	char *p;
-
-	if (!var || !value)
-		return (0);
-
-	buf = malloc(_strlen(var) + _strlen(value) + 2);
-	if (!buff)
-		return (1);
-	_strcpy(buff, var);
-	_strcat(buff, "=");
-	_strcat(buff, value);
-	node = info->env;
-	while (node)
+	if (!info->environ || info->env_changed)
 	{
-		p = starts_with(node->str, var);
-		if (p && *p == '=')
-		{
-			free(node->str);
-			node->str = buff;
-			info->env_changed = 1;
-			return (0);
-		}
-		node = node->next;
+		info->environ = list_to_strings(info->env);
+		info->env_changed = 0;
 	}
-	add_node_end(&(info->env), buff, 0);
-	free(buff);
-	info->env_changed = 1;
-	return (0);
+
+	return (info->environ);
 }
+
 /**
  * _unsetenv - Remove an environment variable
  * @info: Structure containing potential arguments. Used to maintain
@@ -72,19 +48,46 @@ int _unsetenv(info_t *info, char *var)
 	}
 	return (info->env_changed);
 }
-/**
- * get_environ - returns the string array copy of our environ
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- * Return: Always 0
- */
-char **get_environ(info_t *info)
-{
-	if (!info->environ || info->env_changed)
-	{
-		info->environ = list_to_strings(info->env);
-		info->env_changed = 0;
-	}
 
-	return (info->environ);
+/**
+ * _setenv - Initialize a new environment variable,
+ *             or modify an existing one
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ * @var: the string env var property
+ * @value: the string env var value
+ *  Return: Always 0
+ */
+int _setenv(info_t *info, char *var, char *value)
+{
+	char *buf = NULL;
+	list_t *node;
+	char *p;
+
+	if (!var || !value)
+		return (0);
+
+	buf = malloc(_strlen(var) + _strlen(value) + 2);
+	if (!buf)
+		return (1);
+	_strcpy(buf, var);
+	_strcat(buf, "=");
+	_strcat(buf, value);
+	node = info->env;
+	while (node)
+	{
+		p = starts_with(node->str, var);
+		if (p && *p == '=')
+		{
+			free(node->str);
+			node->str = buf;
+			info->env_changed = 1;
+			return (0);
+		}
+		node = node->next;
+	}
+	add_node_end(&(info->env), buf, 0);
+	free(buf);
+	info->env_changed = 1;
+	return (0);
 }
